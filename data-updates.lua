0c617ac52cc2c467e78099a0bff672d4336c1625
kyran-z <m.sarti@htvalue.it>
2026-07-06 10:53:46 +0200

    so much stuff

--- data-updates.lua
+++
@@ -1,5 +0,0 @@
-require("prototypes.items.holmium")
-require("prototypes.items.gleba-science")
-require("prototypes.items.uranium")
-
-require("prototypes.technologies.rocket")
--- data.lua
+++ data.lua
@@ -1,4 +1,13 @@
+-- technologies
 require("prototypes.technologies.tungsten")
 require("prototypes.technologies.holmium")
 require("prototypes.technologies.fruits")
 require("prototypes.technologies.lithium")
+require("prototypes.technologies.rocket")
+
+-- items
+require("prototypes.items.concrete")
+require("prototypes.items.holmium")
+require("prototypes.items.gleba-science")
+require("prototypes.items.uranium")
+require("prototypes.items.fish")
